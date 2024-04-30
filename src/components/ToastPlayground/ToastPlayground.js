479b3d7a9ce4a0aaebfec31f83b4a93c44816cf4
Michael Weber <michael.weber@spring-media.de>
2024-04-30 14:57:46 +0200

    exercise 1 done

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,9 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,39 +27,47 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea value={message}
+              onChange={event => setMessage(event.target.value)}
+              id="message"
+              className={styles.messageInput} />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
 
-            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map(currentVariant => {
+              const key = `variant-${currentVariant}`;
+              return (<>
+                <label htmlFor={key}>
+                  <input
+                    id={key}
+                    type="radio"
+                    name={key}
+                    value={currentVariant}
+                    checked={variant === currentVariant}
+                    onChange={event => {
+                      console.log(event.target.value);
+                      setVariant(event.target.value);
+                    }}
+                  />
+                  {currentVariant}
+                </label>
+              </>)
+            })}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`} >
             <Button>Pop Toast!</Button>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
